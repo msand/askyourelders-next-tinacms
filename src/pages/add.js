a1de640073971b7249727a2207fc95be5940da07
Mikael Sand <msand@abo.fi>
2020-05-05 03:03:42 +0300

    Attempt to fix staticman

--- src/pages/add.js
+++ src/pages/add.js
@@ -131,7 +131,7 @@ export default function Add(props) {
         <div>
           <form
             method="POST"
-            action="/.netlify/functions/staticman/msand/askyourelers-next-tinacms/master/comments"
+            action="/.netlify/functions/staticman/msand/askyourelders-next-tinacms/master/comments"
           >
             <input
               name="options[redirect]"
